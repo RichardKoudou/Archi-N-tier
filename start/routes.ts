b314b884bdfb5d6cde59c544048d86f9e5460769
RichardKoudou <richard.koudou@epsi.fr>
2024-06-17 15:48:44 +0200

    add : CRUD Category + stocks

--- start/routes.ts
+++ start/routes.ts
@@ -8,23 +8,26 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import CategoriesController from '#controllers/categories_controller';
+import StocksController from '#controllers/stocks_controller';
+//import UserController from '#controllers/UserController';
 
 router.on('/').render('pages/home')
 
 // Routes pour les catégories
 router.group(() => {
-    router.get('/all', 'categories_controller.index') // Afficher toutes les catégories
-    router.post('/create', 'categories_controller.store') // Créer une nouvelle catégorie
-    router.get('/:id', 'categories_controller.show') // Afficher une catégorie spécifique
-    router.put('/:id', 'categories_controller.update') // Mettre à jour une catégorie
-    router.delete('/:id', 'categories_controller.destroy') // Supprimer une catégorie
+    router.get('/all', '#controllers/categories_controller.index') // Afficher toutes les catégories
+    router.post('/create', '#controllers/categories_controller.store') // Créer une nouvelle catégorie
+    router.get('/:id', '#controllers/categories_controller.show') // Afficher une catégorie spécifique
+    router.put('/:id', '#controllers/categories_controller.update') // Mettre à jour une catégorie
+    router.delete('/:id', '#controllers/categories_controller.destroy') // Supprimer une catégorie
   }).prefix('categories')
 
 
 router.group(() => {
-    router.get('/', 'stocks_controller.index') // Afficher tous les stocks
-    router.post('/', 'stocks_controller.store') // Créer un nouveau stock
-    router.get('/:id', 'stocks_controller.show') // Afficher un stock spécifique
-    router.put('/:id', 'stocks_controller.update') // Mettre à jour un stock
-    router.delete('/:id', 'stocks_controller.destroy') // Supprimer un stock
+    router.get('/all', '#controllers/stocks_controller.index') // Afficher tous les stocks
+    router.post('/create', '#controllers/stocks_controller.store') // Créer un nouveau stock
+    router.get('/:id', '#controllers/stocks_controller.show') // Afficher un stock spécifique
+    router.put('/:id', '#controllers/stocks_controller.update') // Mettre à jour un stock
+    router.delete('/:id', '#controllers/stocks_controller.destroy') // Supprimer un stock
   }).prefix('stocks')
